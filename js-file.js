e2d7ea08e76160079ef3aafd58e1c56bba5f34df
Andrei Negrea <negrea97@yahoo.com>
2023-02-16 21:20:24 +0200

    Add files via upload
    Test 1 DOM

---
+++ js-file.js
@@ -0,0 +1,25 @@
+const containter = document.querySelector('#conta');
+
+const p = document.createElement('p');
+p.style.color = 'red';
+p.textContent = `Hei, I'm red`;
+containter.appendChild(p);
+
+const h3 = document.createElement('h3');
+h3.textContent = `I'm blue!`;
+h3.style.color = 'blue';
+containter.appendChild(h3);
+
+const div2 = document.createElement('div');
+div2.style.cssText = 'border-color: black; background: pink';
+
+const hunu = document.createElement('h1');
+hunu.textContent = 'Im in a div';
+
+const pdoi = document.createElement('p');
+pdoi.textContent = 'ME TO!';
+
+div2.appendChild(hunu);
+div2.appendChild(pdoi);
+containter.append(div2);
+
